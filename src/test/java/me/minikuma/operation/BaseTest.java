5dab5985b804e81cba42d3d22849bd7fbbc0bc87
minikuma <minikuma@naver.com>
2022-03-01 22:45:20 +0900

    [TEST] spring redis multiple transaction test

--- src/test/java/me/minikuma/operation/BaseTest.java
+++ src/test/java/me/minikuma/operation/BaseTest.java
@@ -2,10 +2,13 @@ package me.minikuma.operation;
 
 import me.minikuma.config.EmbeddedRedisConfig;
 import me.minikuma.config.EmbeddedRedisServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.context.annotation.Import;
 
 @DataRedisTest
 @Import({EmbeddedRedisConfig.class, EmbeddedRedisServer.class})
 public class BaseTest {
+    final Logger LOG = LoggerFactory.getLogger(this.getClass());
 }
--- src/test/java/me/minikuma/operation/MultipleOperations.java
+++ src/test/java/me/minikuma/operation/MultipleOperations.java
@@ -1,12 +1,17 @@
 package me.minikuma.operation;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MultipleOperations extends BaseTest {
 
@@ -18,7 +23,7 @@ public class MultipleOperations extends BaseTest {
         // given
         String key = "multiple-key";
         redisTemplate.opsForValue().set(key, 10);
-        Assertions.assertThat(redisTemplate.opsForValue().get(key)).isEqualTo(10);
+        assertThat(redisTemplate.opsForValue().get(key)).isEqualTo(10);
 
         // when
         redisTemplate.executePipelined(new RedisCallback<Object>() {
@@ -34,6 +39,40 @@ public class MultipleOperations extends BaseTest {
 
         // then
         int totalCount = (int) redisTemplate.opsForValue().get(key);
-        Assertions.assertThat(totalCount).isEqualTo(14);
+        assertThat(totalCount).isEqualTo(14);
+    }
+
+    @Test
+    void 레디스_트랜젝션_처리_등록_테스트() {
+        // given
+        redisTemplate.opsForValue().set("tx1", 10);
+        redisTemplate.opsForSet().add("tx2", "tx2_value");
+        assertThat(redisTemplate.opsForSet().isMember("tx2", "30")).isFalse();
+
+        // when
+        redisTemplate.execute(new SessionCallback<Object>() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public <K, V> Object execute(RedisOperations<K, V> operations) throws DataAccessException {
+                operations.watch((K) "tx1"); // tx1 값 변경 확인
+                operations.multi(); // 트랜젝션 시작
+
+                // 작업
+                operations.opsForValue().increment((K) "tx1", 1); // tx1 값에 1을 증가 시킴
+                operations.opsForSet().add((K) "tx2", (V) "tx2_add_value"); // tx2 키에 추가 값 등록
+
+                return operations.exec(); // 트랜젝션 커밋
+            }
+        });
+
+        // then
+        Object tx1Values = redisTemplate.opsForValue().get("tx1");
+        Set<Object> tx2Values = redisTemplate.opsForSet().members("tx2");
+        Boolean tx2ValueIsChecked = redisTemplate.opsForSet().isMember("tx2", "tx2_add_value");
+        LOG.info("tx1 data = {}", tx1Values);
+        LOG.info("tx2 data = {}", tx2Values);
+
+        assertThat(tx1Values).isEqualTo(11);
+        assertThat(tx2ValueIsChecked).isTrue();
     }
 }
